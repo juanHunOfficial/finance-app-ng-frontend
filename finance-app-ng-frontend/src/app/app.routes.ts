a8a0f364176d24a173298b642e553298302e9518
Juan Hun <juanhun2424@gmail.com>
2025-02-21 09:10:12 -0500

    Add beginning routes for the home component

--- finance-app-ng-frontend/src/app/app.routes.ts
+++ finance-app-ng-frontend/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [{
+    path: '',
+    pathMatch: 'full',
+    loadComponent: () => {
+        return import('./pages/home/home.component').then((m)=> m.HomeComponent);
+    },
+}];
+
+// Notice it is returning a array of objects, similar to JSON. This is the format that Angular uses to define routes.
