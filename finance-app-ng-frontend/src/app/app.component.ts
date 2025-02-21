7a88529108e48b735f9f5e07d1164c0f2bc5c3ec
Juan Hun <juanhun2424@gmail.com>
2025-02-21 10:02:59 -0500

    Add angular MUI package

--- finance-app-ng-frontend/src/app/app.component.ts
+++ finance-app-ng-frontend/src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HomeComponent } from './pages/home/home.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HomeComponent, NavbarComponent, FooterComponent],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
